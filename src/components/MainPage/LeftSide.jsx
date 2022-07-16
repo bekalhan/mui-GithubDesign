ef328204656abadd6695dd1481a335a768b2ec84
root07 <77151270+bekalhan@users.noreply.github.com>
2022-07-16 08:10:00 -0700

    left side completed

--- src/components/MainPage/LeftSide.jsx
+++ src/components/MainPage/LeftSide.jsx
@@ -11,9 +11,12 @@ import {
     Box,
     Typography,
     styled,
-    Button
+    Button,
+    InputBase
   } from "@mui/material";
   import DvrIcon from '@mui/icons-material/Dvr';
+  import cedric from '../../img/ced7.jpeg';
+
 
   const StartDiv = styled("div")(({ theme }) => ({
         display:"flex",
@@ -21,11 +24,26 @@ import {
         
   })); 
 
+  const Search = styled("div")(({ theme }) => ({
+    backgroundColor: "#08040c",
+    border:"1px solid white",
+    marginTop:"1em",
+    padding: "0 10px",
+    borderRadius: theme.shape.borderRadius,
+    width: "70%",
+    height:"30px",
+    marginLeft:"1em",
+    background:"#101414",
+    display:"flex",
+    justifyContent:"center",
+    marginLeft:"2em"
+  }));
+
 
 function LeftSide() {
   return (
     <Box  flex={1} p={2} sx={{ display: { xs: "none", sm: "block" } }}>
-        <Box position="fixed" width={400} height="100vh" sx={{background:"#0d1116"}}>
+        <Box position="fixed" width={300} height="100vh" sx={{background:"#0d1116"}}>
          <StartDiv>
             <Typography color="white" sx={{marginTop:"2rem",marginLeft:"2rem"}}>
                     Recent Repositories
@@ -37,6 +55,72 @@ function LeftSide() {
                     </Typography>
                 </Button>
          </StartDiv>
+         <Search>
+             <InputBase sx={{color:"white",fontWeight:"200"}} placeholder='find a repository'></InputBase>
+         </Search>
+         <List sx={{marginLeft:"1em",marginTop:"1em"}}>
+            <ListItem>
+                <Avatar src={cedric} sx={{width:"20px",height:"20px"}} />
+                <Typography color="white" sx={{marginLeft:"0.3em"}}>
+                    bekalhan/Mern-Stack-Blog-Server
+                </Typography>
+            </ListItem>
+            <ListItem>
+                <Avatar src={cedric} sx={{width:"20px",height:"20px"}} />
+                <Typography color="white" sx={{marginLeft:"0.3em"}}>
+                    bekalhan/Material-UI-WebsiteDesign
+                </Typography>
+            </ListItem>
+            <ListItem>
+                <Avatar src={cedric} sx={{width:"20px",height:"20px"}} />
+                <Typography color="white" sx={{marginLeft:"0.3em"}}>
+                    bekalhan/ECommerceApp-Client
+                </Typography>
+            </ListItem>
+            <ListItem>
+                <Avatar src={cedric} sx={{width:"20px",height:"20px"}} />
+                <Typography color="white" sx={{marginLeft:"0.3em"}}>
+                    bekalhan/ECommerceApp
+                </Typography>
+            </ListItem>
+            <ListItem>
+                <Avatar src={cedric} sx={{width:"20px",height:"20px"}} />
+                <Typography color="white" sx={{marginLeft:"0.3em"}}>
+                    bekalhan/MEVN-Stack--Radice-TheDeveloperUniverse-Prototype
+                </Typography>
+            </ListItem>
+            <ListItem>
+                <Avatar src={cedric} sx={{width:"20px",height:"20px"}} />
+                <Typography color="white" sx={{marginLeft:"0.3em"}}>
+                    bekalhan/MongoDB-Express.js-Vue.js-Node.js-SugIss-App
+                </Typography>
+            </ListItem>
+            {/* <ListItem>
+                <Avatar src={cedric} sx={{width:"20px",height:"20px"}} />
+                <Typography color="white" sx={{marginLeft:"0.3em"}}>
+                    bekalhan/Mui-ResponsiveDesign
+                </Typography>
+            </ListItem>
+            <ListItem>
+                <Avatar src={cedric} sx={{width:"20px",height:"20px"}} />
+                <Typography color="white" sx={{marginLeft:"0.3em"}}>
+                    bekalhan/MusicApp-frontend-React
+                </Typography>
+            </ListItem> */}
+         </List>
+         <Typography color="white" sx={{marginLeft:"2em"}}>
+            Show more
+         </Typography>
+         <Box sx={{marginTop:"1em",width:"80%",marginLeft:"30px",opacity:"3"}}>
+            <hr>
+            </hr>
+         </Box>
+         <Box>
+            <Typography color="white" sx={{fontSize:"15px",marginLeft:"1em",marginRight:"1em",marginTop:"1em"}}>
+                 When you take actions across GitHub, we’ll provide links to that activity here
+            </Typography>
+         </Box>
+
         </Box>
     </Box>
   )
--- src/components/MainPage/MiddleSide.jsx
+++ src/components/MainPage/MiddleSide.jsx
@@ -10,11 +10,27 @@ import {
     useMediaQuery,
     Box
   } from "@mui/material";
+  import cedric from '../../img/ced7.jpeg';
+
 
 function MiddleSide() {
   return (
-    <Box flex={5} p={{ xs: 0, md: 2 }}>
-        MiddleSide
+    <Box flex={2} p={{ xs: 0, md: 2 }}>
+        <Box position="fixed" height="100vh" width={600} sx={{background:"#010409"}}>
+        <Box sx={{display:"flex",justifyContent:"flex-start",marginLeft:"2em"}}> 
+        <List sx={{display:"flex",color:"white",justifyContent:"flex-start",marginTop:"1em"}}>
+                <ListItem sx={{display:"flex",flexDirection:"column"}}>
+                    <ListItemText  primary="PullRequest" />
+                </ListItem>
+                <ListItem sx={{display:"flex",flexDirection:"column"}}> 
+                    <ListItemText primary="Issues" />
+                </ListItem>
+            </List>
+        </Box>
+        <Box sx={{width:"70%",marginLeft:"3em"}}>
+            <hr></hr>
+        </Box>
+        </Box>
     </Box>
   )
 }
